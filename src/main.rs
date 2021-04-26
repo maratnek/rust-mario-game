23805ed0f2a02fa5e6cff67e075ecdd1ae64e70f
MaratWorker <maratn566ek@gmail.com>
2021-04-27 01:03:25 +0300

    position over x y

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,33 @@ use sfml::{
         Sprite, Text, Texture, Transformable,
     },
     window::{Event, Key, Style},
+    system::Vector2f,
 };
 
 use std::time::{Duration, Instant};
 
 #[derive(Debug)]
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Debug)]
 struct Mario {
-    position: f32,
+    position: Position,
 }
 
 impl Mario {
+    fn get_position(&self) -> Vector2f {
+        Vector2f::new(self.position.x, self.position.y)
+    }
     fn back(&mut self) {
         println!("back");
-        self.position -= 1.0;
+        self.position.x -= 1.0;
     }
     fn forward(&mut self) {
         println!("forward");
-        self.position += 1.0;
+        self.position.x += 1.0;
     }
 }
 
@@ -75,7 +85,7 @@ fn main() {
     let mario_go_right_tx2 =
         Texture::from_file_with_rect("resources-mario/Player.png", &rect2).unwrap();
     // mario sprite
-    let mut mario = Mario { position: 0.0 };
+    let mut mario = Mario { position: Position {x:0.0, y:0.0} };
     let mut mario_sprite = Sprite::new();
     mario_sprite.set_texture(&mario_go_right_tx1, true);
     // mario_sprite.set_texture(&mario_texture, true);
@@ -83,52 +93,7 @@ fn main() {
 
     let mut position = 0.0;
     let mut go_state = State::Go1;
-    mario_sprite.set_position((position, 0.0));
-
-    // Create a new texture. (Hey Frank!)
-    let frank = Texture::from_file("resources/frank.jpeg").unwrap();
-
-    // Create a font.
-    let font = Font::from_file("resources/sansation.ttf").unwrap();
-
-    // rectang.set_position(position: P)
-    // Create a circle with the Texture.
-    let mut circle = CircleShape::with_texture(&frank);
-    circle.set_radius(70.0);
-    circle.set_position((100.0, 100.0));
-
-    // Create a Sprite.
-    let mut sprite = Sprite::new();
-    // Have it use the same texture as the circle.
-    sprite.set_texture(&frank, true);
-    sprite.set_position((400.0, 300.0));
-    sprite.set_scale((0.5, 0.5));
-
-    // Create a ConvexShape using the same texture.
-    let mut convex_shape = ConvexShape::with_texture(6, &frank);
-    convex_shape.set_point(0, (400., 100.));
-    convex_shape.set_point(1, (500., 70.));
-    convex_shape.set_point(2, (450., 100.));
-    convex_shape.set_point(3, (580., 150.));
-    convex_shape.set_point(4, (420., 230.));
-    convex_shape.set_point(5, (420., 120.));
-
-    // Create an initialized text using the font.
-    let title = Text::new("Borrowed resources example!", &font, 50);
-
-    // Create a second text using the same font.
-    // This time, we create and initialize it separately.
-    let mut second_text = Text::default();
-    second_text.set_string("This text shares the same font with the title!");
-    second_text.set_font(&font);
-    second_text.set_fill_color(Color::GREEN);
-    second_text.set_position((10.0, 350.0));
-    second_text.set_character_size(20);
-
-    // Create a third text using the same font.
-    let mut third_text = Text::new("This one too!", &font, 20);
-    third_text.set_position((300.0, 100.0));
-    third_text.set_fill_color(Color::RED);
+    mario_sprite.set_position(mario.get_position());
 
     // time
     let mut start = Instant::now();
@@ -236,5 +201,5 @@ fn main() {
 fn update(sprite: &mut Sprite, mario: &mut Mario) {
     // println!("Update {:?} {:?}!", sprite, mario);
     // mario.position += 0.1;
-    sprite.set_position((sprite.position().x, mario.position));
+    sprite.set_position((sprite.position().x, mario.position.x));
 }
